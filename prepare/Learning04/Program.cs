e0b4485dc384cba6aeb25c03943c94601d5e8b83
SharkzCodez <z2leblanc@outlook.com>
2024-07-17 02:50:36 -0500

    w4 prep

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("John Eggbert", "Algebra 1");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Tsukasa Tenma", "Calculus", "5.8", "7-11");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Sans Undertale", "American History", "The Industrial Revolution");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
